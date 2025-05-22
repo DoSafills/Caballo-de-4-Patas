aa190ae1b3c4b946929a720a1d5ee4ed8813c341
ConstanzaOyarzo <coyarzod0@gmail.com>
2025-05-22 17:34:17 -0400

    nombre

--- Clases/Usuario.py
+++ Clases/Usuario.py
@@ -1,8 +1,8 @@
 class Usuario:
-    def __init__(self, rut, nombre, apellido, edad, email, tipo):
+    def __init__(self, rut, nombre, apellido, edad, email, rol):
         self.rut = rut
         self.nombre = nombre
         self.apellido = apellido
         self.edad = edad
         self.email = email
-        self.tipo = tipo
+        self.rol = rol
