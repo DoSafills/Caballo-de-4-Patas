c6e447667399aacaf28f407448f7eb56a55b11c8
Nelson Quiñinao <127786657+Anker04@users.noreply.github.com>
2025-05-27 00:51:16 -0400

    a

--- Clases/Cliente.py
+++
@@ -1,7 +0,0 @@
-from Usuario import Usuario
-
-class Cliente(Usuario):
-    def __init__(self, id_cliente, id_mascota, rut, nombre, apellido, edad, email, tipo):
-        super().__init__(rut, nombre, apellido, edad, email, tipo)
-        self.id_cliente = id_cliente
-        self.id_mascota = id_mascota
--- Clases/Recepcionista.py
+++
@@ -1,7 +0,0 @@
-from Usuario import Usuario
-
-class Recepcionista(Usuario):
-    def __init__(self, id_recepcionista, contrasena, rut, nombre, apellido, edad, email, tipo):
-        super().__init__(rut, nombre, apellido, edad, email, tipo)
-        self.id_recepcionista = id_recepcionista
-        self.contrasena = contrasena
