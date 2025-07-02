5457bf35f26a6b5e5a070b74d09966be1abe3aaf
ConstanzaOyarzo <coyarzod0@gmail.com>
2025-07-02 17:39:48 -0400

    purga

--- Veterinaria/repositories/consulta_repository.py
+++
@@ -1,8 +0,0 @@
-from sqlalchemy.orm import Session
-from Veterinaria.models import Consulta
-from .base_repository import BaseRepository
-
-
-class ConsultaRepository(BaseRepository):
-    def __init__(self, session: Session):
-        super().__init__(session, Consulta)
